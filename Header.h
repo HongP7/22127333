bed0b725493452801792c60264a3bb5a0e7b8a69
PaiteCute hột me <127521457+HongP7@users.noreply.github.com>
2025-02-15 10:47:48 +0700

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+class student{
+    string mssv;
+    string name;
+    DDate ngaysinh;
+    string gioitinh, khoa, Academic_Year, chuong_Trinh, dia_Chi, emai, sdt, tinh trang;
+ 
+};
+
+class Date{
+    int d, m, y;
+};
